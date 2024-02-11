d2cf8276156aaa5b147f53d57e3108fca395bc8f
Abdulaziz Memesh <a.memesh@intelmatix.ai>
2024-02-11 01:51:46 -0500

    initial files

---
+++ main.py
@@ -0,0 +1,8 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import yfinance as yf
+from prophet import Prophet
+import streamlit as st
+
+st.title('Hello World')
+st.write('This is a simple hello world app')
